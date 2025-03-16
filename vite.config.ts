f8c2b949a88954e22769e95b47be01147234464c
ThousandDream <2392492803@qq.com>
2025-03-16 16:37:15 +0800

    feat: 新增包chunk组成脚本

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { visualizer } from "rollup-plugin-visualizer";
 // https://vite.dev/config/
+const VISUALIZER = process.env.VISUALIZER;
 export default defineConfig({
   plugins: [
     react(),
-    visualizer({
-      gzipSize: true,
-      brotliSize: true,
-      emitFile: false,
-      filename: "dist.html", //分析图生成的文件名
-      open: true, //如果存在本地服务端口，将在打包后自动展示
-    }),
+    VISUALIZER
+      ? visualizer({
+          gzipSize: true,
+          brotliSize: true,
+          emitFile: false,
+          filename: "dist.html", //分析图生成的文件名
+          open: true, //如果存在本地服务端口，将在打包后自动展示
+        })
+      : undefined,
   ],
   resolve: {
     alias: {
